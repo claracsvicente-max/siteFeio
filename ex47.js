35c806112acd3424cfeec30a8d7ce4eb6d0349a3
claracsvicente-max <clara.csvicente@gmail.com>
2026-05-22 18:26:45 -0300

    bruno

--- ex47.js
+++ ex47.js
@@ -13,10 +13,27 @@ let s = ler()
 console.log('informe se o filme é nacional')
 let n = ler()
 
+let a = x(i, m, s, n)
+
+console.log(a)
+
 function x(i, m, s, n){
     let tex = ''
 
     if(s == 'quarta' && n == 'nao'){
-        let d =  
+        let d =  (i + m) * 14.25
+        tex = `total a se pagar é R$${d}`
     }
+
+    else if(n == 'sim'){
+        let d2 = (i + m) * 5
+        tex = `o total a se pagar é R$${d2}`
+    }
+
+    else if(s != 'quarta' && n == 'nao'){
+        let d3 = (i * 28.50) + (m * 14.25)
+        tex = `o total a se pagar R$${d3}`
+    }
+
+    return tex
 }
---
+++ ex48.js
@@ -0,0 +1,9 @@
+import prompt from 'prompt-sync'
+let ler = prompt()
+
+console.log('informe seu orçamento')
+let u = Number(ler())
+let s = Number(ler())
+
+let v = u
+let k = s

d31b9680abf4b1d4fcf1a480958850c9d25f5389
claracsvicente-max <clara.csvicente@gmail.com>
2026-05-22 17:34:10 -0300

    bruno

---
+++ ex47.js
@@ -0,0 +1,22 @@
+import prompt from 'prompt-sync'
+let ler = prompt()
+
+console.log(`informe quantos ingressos você quer hoje
+MEIA-ENTRADA
+INTEIRA`)
+let i = Number(ler())
+let m = Number(ler())
+
+console.log('informe o dia da semana')
+let s = ler()
+
+console.log('informe se o filme é nacional')
+let n = ler()
+
+function x(i, m, s, n){
+    let tex = ''
+
+    if(s == 'quarta' && n == 'nao'){
+        let d =  
+    }
+}

66e09d4c4857498d9de52ac84a0ccfeb90922472
clara <clara.csvicente@gmail.com>
2026-05-23 13:12:39 -0300

    bruno

--- ex48.js
+++ ex48.js
@@ -1,9 +1,38 @@
 import prompt from 'prompt-sync'
 let ler = prompt()
 
-console.log('informe seu orçamento')
-let u = Number(ler())
-let s = Number(ler())
+console.log('informe seus ganhos mensais e sues gastos mensais')
+let ganho = Number(ler())
+let gasto = Number(ler())
 
-let v = u
-let k = s
+let x = or(ganho, gasto)
+
+console.log(x)
+
+function or(ganho, gasto){
+    let o = (gasto / ganho) * 100
+
+    let tex = ''
+
+    if(gasto > ganho){
+        tex = 'Orçamento comprometido! Hora de rever seus gastos!'
+    }
+
+    else if(o >= 81 && o <= 100){
+        tex = 'Cuidado, seu orçamento pode ficar comprometido!'
+    }
+
+    else if(o >= 51 && o <= 80){
+        tex = 'Atenção, melhor conter os gastos!'
+    }
+
+    else if(o >= 21 && o <= 50){
+        tex = 'Muito bem, seus gastos não ultrapassam metade dos ganhos!'
+    }
+
+    else if(o >= 0 && 0 <= 20){
+        tex = 'Parabéns, está gerenciando bem seu orçamento!'
+    }
+
+    return tex
+}
